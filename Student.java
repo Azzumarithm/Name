5e67df897710b22ed90f929a6e139f5561a75801
azzumar247 <azzumar.tbskh@gmail.com>
2022-10-13 12:27:54 +0800

    First Commit

---
+++ Student.java
@@ -0,0 +1,11 @@
+public class Student {
+	public static void main(String[] args) {
+
+		System.out.println("MUHAMMAD AZZUMAR BIN TASNEEM BUKHARI");
+		System.out.println("210088");
+		System.out.println("KOLEJ CANSELOR");
+
+
+}
+
+}
